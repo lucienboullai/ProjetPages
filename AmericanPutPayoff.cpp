2fc09357190f9d333ea8fb75659a5ea1cbe9f15d
Lucien Boullai <Lucien Boullai>
2016-03-19 18:24:44 +0100

    first commit

---
+++ AmericanPutPayoff.cpp
@@ -0,0 +1,19 @@
+#include "AmericanPutPayoff.h"
+
+
+AmericanPutPayoff::AmericanPutPayoff(double strike, double maturity, int N, int b, int M) : 
+Payoff(N, b, M),
+strike(strike) 
+{
+	americanStyle = true;
+}
+
+void AmericanPutPayoff::evaluate(double* spots, double* payoffs)
+{
+	for (int j = 0; j < b*N; j++)
+		payoffs[j] = std::max(0.0, strike - spots[j]);
+}
+
+AmericanPutPayoff::~AmericanPutPayoff()
+{
+}
